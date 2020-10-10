c07b2e9eab3f17dfa1ce87023377640639fd3c9b
Andrey Petrov <3314099@mail.ru>
2020-10-10 14:56:08 +0300

    ready handlebars

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
 
+const hbs = exphbs.create({
+	defaultLayout: 'main',
+	extname: 'html'
+})
+
+app.engine('html', hbs.engine)
+app.set('view engine', 'html')
+app.set('views', 'views')
+
 app.get('/', (req, res) => {
-	res.status(200)
-	res.sendFile(path.join(__dirname, 'views', 'index.html'))
+	res.render('index')
 })
 
 app.get('/about', (req, res) => {
-	res.status(200)
-	res.sendFile(path.join(__dirname, 'views', 'about.html'))
+	res.render('about')
 })
 
 const PORT = process.env.PORT || 3000
